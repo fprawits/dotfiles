3df625d84f5baa0b0e4855ccbde088b1fd9b80b6
fprawits <prawits.florian@gmail.com>
2019-05-12 00:49:20 +0200

    Add zathurarc to dotfiles and update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,17 +5,24 @@
 # largely taken from:
 # https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
 
-dir=$HOME/.dotfiles
-files="bashrc bash_aliases \
+mkdir -p "$HOME/.ssh/"
+mkdir -p "$HOME/.vim/" "$HOME/.vim/ftplugin/"
+mkdir -p "$HOME/.config/zathura/"
+
+dir="$HOME/.dotfiles"
+files="bashrc \
+    bash_aliases \
+    config/zathura/zathurarc \
     ssh/config \
-    vimrc gvimrc \
+    gvimrc \
+    vimrc \
     vim/ftplugin/tex.vim \
     vim/ftplugin/latex.vim \
     vim/ftplugin/text.vim"
 
 for file in $files; do
     echo "Creating symlink to $file in $HOME ..."
-    ln -sf $dir/$file $HOME/.$file
+    ln -sf "${dir}/$file" "${HOME}/.$file"
 done
 
 echo "...done"
