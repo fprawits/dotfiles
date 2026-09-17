ae191216901997755dc8970d50ec8b455d64543d
prawitsf <florian.prawits@ait.ac.at>
2026-09-17 14:43:09 +0200

    Update bash script makesymlinks.sh
    Report errors to stderr instead of stdout
    Retab the script to use spaces instead of tabs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,6 +2,8 @@
 #
 # Update symlinks in $HOME via GNU stow to apply configuration files in this
 # repository.
+# NOTE: currently this script only manages packages actively tracked in git (by
+# inspecting git's HEAD).
 
 # bash 'strict' mode
 set -euo pipefail
@@ -9,20 +11,21 @@ IFS=$'\n\t'
 
 if ! command -v stow &> /dev/null
 then
-	echo "Required program GNU stow not found!"
-	echo "Aborting"
-	exit 1
+     printf '%s\n' \
+        "Required program GNU Stow not found!" \
+        "Aborting." >&2
+    exit 1
 fi
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 DOTFILE_DIR="$PWD"
 
 for d in $(git ls-tree -d --name-only HEAD); do
-	stow --restow --verbose=1 --dir="$DOTFILE_DIR" --target="$HOME" "$d"
+    stow --restow --verbose=1 --dir="$DOTFILE_DIR" --target="$HOME" "$d"
 done
 
 if [ ! -f ~/.vim/systags ]; then
-	echo -n "Generating tagfile for system C headers ... "
-	bash ~/.vim/generate-systags.sh
-	echo "done"
+    echo -n "Generating tagfile for system C headers ... "
+    bash "$HOME/.vim/generate-systags.sh"
+    echo "done"
 fi
